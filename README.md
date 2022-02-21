b974aa75795f0f76ff69b79d855ee24ca0ecd62e
Lee <leevashan.reddi@aax.co.za>
2022-02-21 22:44:52 +0200

    updated with working sample

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ run `php -S localhost:4200`
 
 1. [P5 JS](https://p5js.org/)
 2. [ASCII Video using P5js](https://www.youtube.com/watch?v=55iwMYv8tGI)
+3. [ASCII Play](https://play.ertdfgcvb.xyz/)
--- app.js
+++ app.js
@@ -1,7 +1,30 @@
+const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+
+let pic;
+
+function preload() {
+    pic = loadImage("photo-text-test.jpg");
+}
+
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-  }
+    noCanvas();
+    background(0);
+    let w = width / pic.width;
+    let h = height / pic.height;
+    pic.loadPixels();
+    for (let j = 0; j < pic.height; j++) {
+        let row = '';
+        for (let i = 0; i < pic.width; i++) {
+            const pixelIndex = (i + j * pic.width) * 4;
+            const r = pic.pixels[pixelIndex + 0];
+            const g = pic.pixels[pixelIndex + 1];
+            const b = pic.pixels[pixelIndex + 2];
+            const avg = (r + g + b) / 3;
+            const len = density.length;
+            const charIndex = floor(map(avg, 0, 255, len, 0));
+            const c = density.charAt(charIndex);
+            row+= c == ' ' ? '&nbsp;': c;
+        }
+        createDiv(row);
+    }
+}
