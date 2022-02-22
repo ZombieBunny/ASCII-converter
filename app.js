32915f76e95e93366cd4eca6808c6b1fb03b129e
Lee <leevashan.reddi@aax.co.za>
2022-02-22 20:19:52 +0200

    updated with video stream

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
 
-let pic;
+let pic, video, asciiDiv;
 
-function preload() {
-    pic = loadImage("photo-text-test.jpg");
-}
+// function preload() {
+//     pic = loadImage("photo-text-test.jpg");
+// }
 
 function setup() {
     noCanvas();
-    background(0);
-    let w = width / pic.width;
-    let h = height / pic.height;
-    pic.loadPixels();
-    for (let j = 0; j < pic.height; j++) {
-        let row = '';
-        for (let i = 0; i < pic.width; i++) {
-            const pixelIndex = (i + j * pic.width) * 4;
-            const r = pic.pixels[pixelIndex + 0];
-            const g = pic.pixels[pixelIndex + 1];
-            const b = pic.pixels[pixelIndex + 2];
+    video = createCapture(VIDEO);
+    video.size(48,48);
+    asciiDiv = createDiv();
+}
+
+function draw() {
+    // background(0);
+    // let w = width / pic.width;
+    // let h = height / pic.height;
+    video.loadPixels();
+    let asciiImage = '';
+    for (let j = 0; j < video.height; j++) {
+        for (let i = 0; i < video.width; i++) {
+            const pixelIndex = (i + j * video.width) * 4;
+            const r = video.pixels[pixelIndex + 0];
+            const g = video.pixels[pixelIndex + 1];
+            const b = video.pixels[pixelIndex + 2];
             const avg = (r + g + b) / 3;
             const len = density.length;
             const charIndex = floor(map(avg, 0, 255, len, 0));
             const c = density.charAt(charIndex);
-            row+= c == ' ' ? '&nbsp;': c;
+            asciiImage+= c == ' ' ? '&nbsp;': c;
         }
-        createDiv(row);
+        // createDiv(row);
+        asciiImage += '<br/>';
     }
+    asciiDiv.html(asciiImage);
 }
